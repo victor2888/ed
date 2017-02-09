48ddc45a0d4a93b6889106b0f331f0dec1240cb9
victor2888 <dam.vgarcia@institutoserpis.org>
2017-02-09 13:41:47 +0100

    introduccion a gtk con monodevelop

--- CGtk/CGtk/MainWindow.cs
+++ CGtk/CGtk/MainWindow.cs
@@ -6,24 +6,37 @@ public partial class MainWindow: Gtk.Window
 	public MainWindow (): base (Gtk.WindowType.Toplevel)
 	{
 		Build ();
+
+		Console.WriteLine("27/10 = "+ 27/10 + "resto = " + 27%10) 
+
+
 		labelSaludo.Text = "Hola. Pon tu nombre y pulsa Adelante";
 
+		//Table table = new Table (9, 10, true);
+		//for (uint vez = 0; index< 90; index++) {
+			//Button button = new Button ();
+			//button.Label = " " + (index +1);
+			//button.Visible = true;
+			//uint row = index/10;
+			//uint column = index % 10;
+			//table.Attach (button, column,column + 1, 0, 1);
+			//button.Clicked += OnButtonGoForwardClicked;
 
 		Table table = new Table (9, 10, true);
-		table.Visible = true;
-		vBox1Main.Add (Table);
-
-		for (uint vez = 0; vez< 5; vez++) {
-			Button button = new Button ();
-			button.Label = " " + vez;
-			button.Visible = true;
-			table.Attach (button, vez, vez + 1, 0, 1);
-			vBox1Main.Add (button);
-			button.Clicked += OnButtonGoForwardClicked;
-			
+		for (uint row = 0; row< 90; row++) {
+			for(uint column = 0; column < 10; column)
+				uint  indexer = row *10 +Column;
+				Button button = new Button ();
+				button.Label = " " + (index +1);
+				button.Visible = true;
+				uint row = index/10;
+				uint column = index % 10;
+				table.Attach (button, column,column + row, row +1);
+				button.Clicked += OnButtonGoForwardClicked;
 
 		}
-
+		table.Visible = true;
+		vBoxMain.Add (table);
 	}
 
 	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
